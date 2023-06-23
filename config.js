067129e415bdb94f6ce75a1608b2efc5c128796e
Hsien Heng Lie <hsien@bbd.co.za>
2023-06-23 17:17:36 +0200

    merge commit

--- config.js
+++ config.js
@@ -6,14 +6,12 @@ const path = './client/js/config.json';
 function writeConfig(){
   if (fs.existsSync(path)) {
     const m = JSON.parse(fs.readFileSync(path));
-    console.log(m.Identity_Server_Base_Url);
     m.Identity_Server_Base_Url = process.env.Identity_Server_Base_Url;
     fs.writeFileSync(path, JSON.stringify(m));
   } else {
     const param = JSON.stringify({
       Identity_Server_Base_Url:process.env.Identity_Server_Base_Url
     });
-    console.log(param);
     fs.writeFile(path, param, function (err) {
       if (err) throw err;
     });
